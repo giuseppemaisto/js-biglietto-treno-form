a686a5f877e21a1d8dfbac254bb72d52c1307d32
giuseppemaisto <gmaisto98@gmail.com>
2022-11-28 16:02:29 +0100

    inserite tutti possibili sconti

--- js/script.js
+++ js/script.js
@@ -8,10 +8,27 @@ BottoneGenera.addEventListener("click", function(){
     const km = document.getElementById("km").value;
     console.log(km);
 
-    const fascia_eta = document.getElementById("fascia-eta").value;
-    console.log(fascia_eta);
+    const fasciaEta = document.getElementById("fascia-eta").value;
+    console.log(fasciaEta);
 
 
     const prezzo = km * 0.21;
-    console.log("il prezzo è " + prezzo)
+   
+
+
+    if(fasciaEta == "minorenne"){
+        let sconto = prezzo * 0.80;
+        
+        console.log( "il prezzo è " + sconto)
+    }else if(fasciaEta == "over-65"){
+        let sconto = prezzo * 0.60;
+        
+        console.log( "il prezzo è " + sconto)
+    }
+    else{
+        sconto = prezzo;
+        console.log( "il prezzo è " + sconto)
+    }
+   
+
 })
